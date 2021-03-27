17da336cedabba0f09924c19de1d5116f18e657d
sudevansujit <dhruvjit1311@gmail.com>
2021-03-27 12:38:40 +0530

    Users can see single Playlists

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, url_for 
-
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Playlister
@@ -74,7 +74,11 @@ def playlists_submit():
     return redirect(url_for('playlists_index'))
 
 
-
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 
 
