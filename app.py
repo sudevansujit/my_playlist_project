64bc4eb9c86ed5fd3e836b63a6c1243971ab41a4
sudevansujit <dhruvjit1311@gmail.com>
2021-03-27 13:06:00 +0530

    Users can edit, update, and destroy playlists

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def video_url_creator(id_lst):
     for vid_id in id_lst:
         # We know that embedded YouTube videos always have this format
         video = 'https://youtube.com/embed/' + vid_id
+        
         videos.append(video)
     return videos
 
@@ -71,14 +72,48 @@ def playlists_submit():
         'video_ids': video_ids
     }
     playlists.insert_one(playlist)
+    "\n".join(playlist.video_ids)
     return redirect(url_for('playlists_index'))
 
 
-@app.route('/playlists/<playlist_id>')
-def playlists_show(playlist_id):
-    """Show a single playlist."""
+
+# @app.route('/playlists/<playlist_id>')
+# def playlists_show(playlist_id):
+#     """Show a single playlist."""
+#     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+#     return render_template('playlists_show.html', playlist=playlist)
+
+
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    # create our updated playlist
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    # set the former playlist to the new one we just updated/edited
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    # take us back to the playlist's show page
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
+    # Add the title parameter here
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+
+
 
 
 
