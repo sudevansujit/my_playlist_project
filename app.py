f78a02b62f9d8800639283ff9840e75bc32caca0
sudevansujit <dhruvjit1311@gmail.com>
2021-03-27 12:22:12 +0530

    Users can create Playlists

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for 
+
 
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
 
-
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        # We know that embedded YouTube videos always have this format
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
 
 app = Flask(__name__)
 
@@ -17,13 +25,68 @@ def index():
 
 
 # OUR MOCK ARRAY OF PROJECTS
-# playlists = [
-# { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
+playlists = [
+{ 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
 
 @app.route('/playlists')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists)
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+# @app.route('/playlists', methods=['POST'])
+# def playlists_submit():
+#     """Submit a new playlist."""
+#     print(request.form.to_dict())
+#     return redirect(url_for('playlists_index'))
+
+# @app.route('/playlists', methods=['POST'])
+# def playlists_submit():
+#     """Submit a new playlist."""
+#     playlist = {
+#         'title': request.form.get('title'),
+#         'description': request.form.get('description')
+#     }
+#     playlists.insert_one(playlist)
+#     return redirect(url_for('playlists_index'))
+
+# Note the methods parameter that explicitly tells the route that this is a POST
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    # Grab the video IDs and make a list out of them
+    video_ids = request.form.get('video_ids').split()
+    # call our helper function to create the list of links
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
